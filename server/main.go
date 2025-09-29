679d811d153fc81facb2a86ac8f1bec682933f54
Maximo <maximoreynosob@gmail.com>
2025-09-29 09:26:26 -0300

    Servidor corregido

--- server/main.go
+++ server/main.go
@@ -5,13 +5,13 @@ import (
 	"net/http"
 )
 
-func main(){
+func main() {
 	index := "./pages"
 	fileServer := http.FileServer(http.Dir(index))
 	http.Handle("/", fileServer)
 
 	port := ":8080"
-	fmt.Printf("Servidor con formulario escuchando en http://localhost%s\n", port)
+	fmt.Printf("Servidor escuchando en http://localhost%s\n", port)
 	err := http.ListenAndServe(port, nil)
 	if err != nil {
 		fmt.Printf("Error: %s\n", err)
